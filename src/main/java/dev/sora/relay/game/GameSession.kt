18bb0290d5e6e68a43556cbc045a0901f1ae4ee7
isuckatcodingfr <161961506+isuckatcodingfr@users.noreply.github.com>
2024-03-03 04:43:38 +0600

    Update GameSession.kt

--- src/main/java/dev/sora/relay/game/GameSession.kt
+++ src/main/java/dev/sora/relay/game/GameSession.kt
@@ -226,7 +226,7 @@ class GameSession : MinecraftRelayPacketListener {
 	}
 
     companion object {
-        const val RECOMMENDED_VERSION = "1.20.0"
+        const val RECOMMENDED_VERSION = "1.20.60"
 		const val COLORED_NAME = "§9§lProtoHax§r"
     }
 }
